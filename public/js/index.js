8e94cbafe57013135eecd487226ec42e8bf2cdc6
willian-ms <willian.ms@icloud.com>
2022-10-13 16:21:21 -0300

    crudzera-clientes

--- public/js/index.js
+++ public/js/index.js
@@ -14,7 +14,14 @@ $(document).ready(function () {
                 columns:[
                     {'data':'id'},
                     {'data':'nome'},
-                    {'data':'cpf'}
+                    {'data':'cpf'},
+                    {'data':'email'},
+                    {'data':'cep'},
+                    {'data':'endereco'},
+                    {'data':'numero'},
+                    {'data':'cidade'},
+                    {'data':'estado'}
+
                 ],
             responsive: true,
             select: true,
@@ -32,14 +39,41 @@ $(document).ready(function () {
 
     $('#tablePerson').on('click', 'tr', function () {
         var data = $('#tablePerson').DataTable().row( this ).data()
-        $("#updateModal").find('#idmodal').val(data["id"])
         $("#deleteModal").find('#idmodal').val(data["id"])
+        $("#updateModal").find('#idmodal').val(data["id"])
         $("#updateModal").find('#modalname').val(data["nome"])
         $("#updateModal").find('#modalcpf').val(data["cpf"])
+        $("#updateModal").find('#modalemail').val(data["email"])
+        $("#updateModal").find('#modalcep').val(data["cep"])
+        $("#updateModal").find('#modaladdress').val(data["endereco"])
+        $("#updateModal").find('#modalnumberAddress').val(data["numero"])
+        $("#updateModal").find('#modalcity').val(data["cidade"])
+        $("#updateModal").find('#modalstate').val(data["estado"])
         $('#updateModal').modal("show");
         console.log(data)
     });
 
+    //Regex CPF
+    document.getElementById('cpf').addEventListener('input', function (e) {
+      var x = e.target.value.replace(/\D/g, '').match(/(\d{0,3})(\d{0,3})(\d{0,3})(\d{0,2})/);
+      e.target.value = !x[2] ? x[1] : x[1] + '.' + x[2] + '.' + x[3] + '-' + x[4] + (x[5] ? '-' + x[5] : '');
+      });
+
+    document.getElementById('modalcpf').addEventListener('input', function (e) {
+      var x = e.target.value.replace(/\D/g, '').match(/(\d{0,3})(\d{0,3})(\d{0,3})(\d{0,2})/);
+      e.target.value = !x[2] ? x[1] : x[1] + '.' + x[2] + '.' + x[3] + '-' + x[4] + (x[5] ? '-' + x[5] : '');
+      });
+    
+    //Regex CEP
+    document.getElementById('cep').addEventListener('input', function (e) {
+      var x = e.target.value.replace(/\D/g, '').match(/(\d{0,5})(\d{0,3})/);
+      e.target.value = !x[2] ? x[1] : x[1] + '-' + x[2];
+      });  
+      
+    document.getElementById('modalcep').addEventListener('input', function (e) {
+      var x = e.target.value.replace(/\D/g, '').match(/(\d{0,5})(\d{0,3})/);
+      e.target.value = !x[2] ? x[1] : x[1] + '-' + x[2];
+      });  
 })
 
 function deleteModal(){
--- routes.js
+++ routes.js
@@ -20,8 +20,14 @@ module.exports = function(app,connection){
   app.post("/newPerson",(req, res) => {
     let name = req.body.name
     let cpf = req.body.cpf
+    let email = req.body.email
+    let cep = req.body.cep
+    let address = req.body.address
+    let numberAddress = req.body.numberAddress
+    let city = req.body.city
+    let state = req.body.state
 
-    var select_query = "INSERT INTO clientes (nome,cpf) VALUES ('"+name+"','"+cpf+"')";
+    var select_query = "INSERT INTO clientes (nome,cpf,email, cep, endereco, numero, cidade, estado) VALUES ('"+name+"','"+cpf+"','"+email+"','"+cep+"','"+address+"','"+numberAddress+"','"+city+"','"+state+"')";
     console.log(select_query);
     connection.query(select_query,function(err, recordset){
       if (err) {
@@ -33,13 +39,19 @@ module.exports = function(app,connection){
   })
 
   app.post('/updatePerson',(req,res) =>{
+    let id = req.body.idmodal
     let name = req.body.modalname
     let cpf = req.body.modalcpf
-    let id = req.body.idmodal
+    let email = req.body.modalemail
+    let cep = req.body.modalcep
+    let address = req.body.modaladdress
+    let numberAddress = req.body.modalnumberAddress
+    let city = req.body.modalcity
+    let state = req.body.modalstate
 
     console.log(name)
 
-    var select_query = "UPDATE clientes SET nome='"+name+"', cpf='"+cpf+"' WHERE id='"+id+"'"
+    var select_query = "UPDATE clientes SET nome='"+name+"', cpf='"+cpf+"',email='"+email+"',cep='"+cep+"',endereco='"+address+"',numero='"+numberAddress+"',cidade='"+city+"',estado='"+state+"' WHERE id='"+id+"'"
     console.log(select_query);
     connection.query(select_query,function(err, recordset){
       if (err) {
